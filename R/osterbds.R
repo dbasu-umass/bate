6c88def40edbb38192e837f3bdee535af58c1fb9
Deepankar Basu <dbasu@umass.edu>
2022-03-02 14:50:41 -0500

    Output of osterbds is a dataframe, not a list

--- R/osterbds.R
+++ R/osterbds.R
@@ -3,7 +3,7 @@
 #' @param parameters A vector of parameters that is generated after estimating the short, intermediate and auxiliary regressions. 
 #' @param Rmax A real number which lies between Rtilde (R-squared for the intermediate regression) and 1. 
 #'
-#' @return A list with three elements:
+#' @return A data frame with three columns:
 #' \item{Discriminant}{The value of the discriminant of the quadratic equation that is solved to generate the identified set}
 #' \item{Interval1}{The interval formed with the first root of the quadratic equation}
 #' \item{Interval2}{The interval formed with the first root of the quadratic equation}
