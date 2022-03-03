11b91ba8db2a00d8159871f7c78b7e59934b22cc
Deepankar Basu <dbasu@umass.edu>
2022-03-03 18:46:25 -0500

    Created data sets and documentation

---
+++ R/data.R
@@ -0,0 +1,53 @@
+#' NLSY IQ.
+#'
+#' NLSY data to analyse the effect of maternal behaviour on 
+#' children's IQ score. Natality detail files are from 2001 and 2002. 
+#' Data is from the NLSY Children and Young Adults panel.
+#' 
+#' @format A data frame with twelve variables:
+#' \describe{
+#' \item{\code{id_std}}{standardized IQ score, PIAT score}
+#' \item{\code{BF_months}}{months of breast feeding}
+#' \item{\code{mom_drink_preg_all}}{did mother drink at all during pregnancy}
+#' \item{\code{lbw_preterm}}{low birth weight + preterm}
+#' \item{\code{age}}{age of child}
+#' \item{\code{female}}{child female}
+#' \item{\code{black}}{mother black}
+#' \item{\code{motherAge}}{age of mother}
+#' \item{\code{mom_married}}{is the mother married?}
+#' \item{\code{income}}{annual income of mother}
+#' \item{\code{sex}}{child sex}
+#' \item{\code{race}}{race of mother}
+#' }
+#'
+#' Source: \url{https://drive.google.com/file/d/1O1W9dP8F3B1DnAZGBegpoqCfysUrn7Uc/view?usp=sharing}
+#'
+"NLSY_IQ"
+
+#' NLSY Birth Weight.
+#'
+#' NLSY data to analyse the effect of maternal behaviour on 
+#' children's birth weight. Natality detail files are from 2001 and 2002. 
+#' Data is from the NLSY Children and Young Adults panel.
+#' 
+#' @format A data frame with twelve variables:
+#' \describe{
+#' \item{\code{birth_wt}}{birth weight, grams}
+#' \item{\code{BF_months}}{months of breast feeding}
+#' \item{\code{mom_drink_preg_all}}{did the mother drink at all during pregnancy}
+#' \item{\code{lbw_preterm}}{low birth weight + preterm}
+#' \item{\code{age}}{age of child}
+#' \item{\code{female}}{child female}
+#' \item{\code{black}}{mother black}
+#' \item{\code{motherAge}}{age of mother}
+#' \item{\code{mom_married}}{is the mother married?}
+#' \item{\code{income}}{annual income of mother}
+#' \item{\code{sex}}{years of schooling of mother}
+#' \item{\code{race}}{race of mother}
+#' \item{\code{gesweek}}{gestation week}
+#' \item{\code{any_smoke}}{did the mother smoke at all during pregnancy}
+#' }
+#'
+#' Source: \url{https://drive.google.com/file/d/1O1W9dP8F3B1DnAZGBegpoqCfysUrn7Uc/view?usp=sharing}
+#'
+"NLSY_BW"
